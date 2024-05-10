7e0915aa8e3f8b85f824f7b13e7c1efa5c663fd3
Ayumu Tanaka <63719198+ayumu-tanaka@users.noreply.github.com>
2024-05-10 22:25:12 +0900

    posit

---
+++ 8176658/packages.R
@@ -0,0 +1,24 @@
+
+# よく使うパッケージを予めインストールしておく。
+
+# 文書作成
+install.packages("rmarkdown")
+install.packages("knitr")
+
+# データ読み込み
+install.packages("readr")
+
+# データ整形・グラフ 
+install.packages("tidyverse")
+install.packages("ggplot2") #"tidyverse"をインストールすれば通常"ggplot2"のインストールは不要
+install.packages("plotly")
+install.packages("doBy")
+
+# 推定
+install.packages("stargazer")
+install.packages("fixest")
+install.packages("modelsummary")
+
+# 地図系
+install.packages("rworldmap")
+
